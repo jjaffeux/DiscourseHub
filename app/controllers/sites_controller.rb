96dd0979a067e70e3e89549b4ba895bd1e32fcee
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2017-01-03 20:26:26 +0100

    implements skeleton controller

---
+++ app/controllers/sites_controller.rb
@@ -0,0 +1,5 @@
+class SitesController < ApplicationController
+  def index
+    render json: []
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+
+  get '/sites', to: "sites#index"
   root to: 'front#index'
 end
